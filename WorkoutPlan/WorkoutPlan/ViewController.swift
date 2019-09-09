bd6c2d8c4638228505ceb2983495b8a4686f45c7
Martin Rist <mrist@mrist-mbp.local>
2019-09-09 14:31:28 +0100

    Initial commit - blank new project

---
+++ WorkoutPlan/WorkoutPlan/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WorkoutPlan
+//
+//  Created by Martin Rist on 09/09/2019.
+//  Copyright © 2019 Martin Rist. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
