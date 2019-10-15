fa2b8a55934c4bcf98e17ab2374502d346f7d3f4
Martin Rist <mlr-github@martinrist.com>
2019-10-15 13:27:39 +0100

    Add basic hard-coded list of exercises

---
+++ WorkoutDiary/WorkoutDiary/Model/Exercise.swift
@@ -0,0 +1,21 @@
+//
+//  Exercise.swift
+//  WorkoutDiary
+//
+//  Created by Martin Rist on 15/10/2019.
+//  Copyright © 2019 Martin Rist. All rights reserved.
+//
+
+import Foundation
+
+class Exercise {
+
+  var name: String
+  var description: String
+
+  init(name: String, description: String) {
+    self.name = name
+    self.description = description
+  }
+
+}
--- WorkoutDiary/WorkoutDiary/Support/AppDelegate.swift
+++ WorkoutDiary/WorkoutDiary/Support/AppDelegate.swift
@@ -12,6 +12,22 @@ import CoreData
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
+  // MARK: - Properties
+
   var window: UIWindow?
+  let dataModel = DataModel()
+
+  // MARK: - Lifecycle
+
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil)
+    -> Bool {
+
+      if let navigationController = window!.rootViewController as? UINavigationController,
+        let controller = navigationController.viewControllers[0] as? ExerciseListViewController {
+        controller.dataModel = dataModel
+      }
+      return true
+  }
 
 }
